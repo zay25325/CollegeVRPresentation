257a94d6473121b566d72753f4b0b84c5ddfcc24
zay25325 <60016762+zay25325@users.noreply.github.com>
2025-03-09 23:08:59 -0400

    Updated the exercise

--- VRPresentation/Assets/Scripts/ExerciseToDoList.cs
+++ VRPresentation/Assets/Scripts/ExerciseToDoList.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class ExerciseToDoList : MonoBehaviour
 {
-    [Header("level 1")]
-    [Tooltip("The left hand isn't tracking. Find ")]
-    public bool FixXRRig;
+    [Header("Section 1")]
 
-    [Header("level 2")]
+    [Tooltip("Learn controls of the device simulator before trying to fix things. Look around, learn to teleport, grab some of the cubes")]
+    public bool LearnDeviceSimulatorControls;
+
+    [Tooltip("Fix the anchor inbetween levels 1 and 2 to make it across the large gap")]
+    public bool FixTeleportationAnchor;
+
+    [Header("Section 2")]
+    [Tooltip("Make the cube in level 2 grabbable and place it in the socket interactor")]
     public bool MakeCubeGrabbable;
 
-    [Header("level 3")]
+    [Header("Section 3")]
+    [Tooltip("Make the UI button interact with XR interactors")]
     public bool AddUIInteraction;
 
-    [Header("level 4")]
+    [Tooltip("Make the push button interactable")]
+    public bool FixPushButtonInteraction;
+
+    [Tooltip("Add the event to the button so it removes the wall")]
+    public bool MakePushButtonTriggerEvent;
+
+
+    [Header("Section 4")]
+
+    [Tooltip("Add affordances back onto the cube, then place it in the checker socket")]
     public bool AddAffordanceToCube;
 }
